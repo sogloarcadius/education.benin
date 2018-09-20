90ee904ec5908daec077c80ff84500ba0a9813ed
sogloarcadius <rtsoglo@gmail.com>
2018-09-20 18:16:41 +0200

    update webapp

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -29,8 +29,8 @@ module.exports = {
 				loader: 'babel-loader',
 				exclude: /node_modules/,
 				query: {
-          presets: ['es2015']
-        }
+          			presets: ['es2015']
+        		}	
 			},
 			{
 				test: /\.css$/,
