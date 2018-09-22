c00afe27e85878621e17a0b9b2a5428570021325
sogloarcadius <rtsoglo@gmail.com>
2018-09-22 18:55:04 +0200

    update urls settings

--- openapi/openapi/urls.py
+++ openapi/openapi/urls.py
@@ -13,8 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, path
+from django.urls import include, path, url
+from django.contrib import admin
+from django.conf import settings
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 
 urlpatterns = [
-    path('', include('api.urls')),
+    url(r'^', include('api.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
